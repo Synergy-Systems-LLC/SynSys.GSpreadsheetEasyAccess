b1071c13367ce7d7abce86c6c8631eb6bcae672b
PavelAltynnikov <Robenzon11@gmail.com>
2020-12-08 16:02:12 +0400

    (chore) added comments to Program

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using GetGoogleSheetDataAPI;
 
 namespace TestConsoleApp
@@ -11,6 +10,9 @@ namespace TestConsoleApp
         static void Main(string[] args)
         {
             var connector = new Connector();
+            // Попытка подключиться к приложению на Google Cloud Platform.
+            // Метод вернёт флаг подключения и у экземпляра коннектора будет возможность
+            // вызывать методы для получения листов google таблиц.
             bool isConnectionSuccessful = connector.TryToCreateConnect(CredentialStream());
 
             if (!isConnectionSuccessful)
@@ -31,16 +33,28 @@ namespace TestConsoleApp
 
             string url = "https://docs.google.com/spreadsheets/d/1dxPz9MEeJxfZkbAvZLE33YLIN5GaNS0Bvqzlp6rAiNk/edit#gid=0";
 
+            // Попытка получения листа по url.
+            // Метод вернёт флаг получения листа и экземпляр типа Sheet.
+            // Если данные из листа не были получены,
+            // то экземпляр sheet будет пустым со статусом в виде строки,
+            // говорящем о причине неудачи.
+            // В противном случае экземпляр листа будет заполнен данными
+            // и эти данные можно будет менять и через конннектор обновлять в листе google таблицы.
             if (connector.TryToGetSheet(url, out Sheet sheet))
             {
                 PrintSheet(sheet);
 
+                // Изменнения данных в экземпляре типа Sheet.
+                // Не важен порядок изменения экземпляра листа,
+                // его обновленние будет происходить в определённом порядке.
                 AddRows(sheet);
                 ChangeRows(sheet);
                 DeleteRows(sheet);
 
                 PrintSheet(sheet);
 
+                // Метод для обновления данных в листе google таблицы на основе измененний
+                // экземпляра типа Sheet.
                 connector.UpdateSheet(sheet);
             }
             else
@@ -53,6 +67,7 @@ namespace TestConsoleApp
 
         private static void ChangeRows(Sheet sheet)
         {
+            // Можно было это делать в цикле например, но так проще.
             sheet.Rows[3].Cells[5].Value = "360";
             sheet.Rows[4].Cells[5].Value = "460";
             sheet.Rows[7].Cells[2].Value = "730";
@@ -62,6 +77,7 @@ namespace TestConsoleApp
 
         private static void DeleteRows(Sheet sheet)
         {
+            // Можно было это делать в цикле например, но так проще.
             sheet.DeleteRow(sheet.Rows[3]);
             sheet.DeleteRow(sheet.Rows[2]);
             sheet.DeleteRow(sheet.Rows[8]);
@@ -69,8 +85,11 @@ namespace TestConsoleApp
 
         private static void AddRows(Sheet sheet)
         {
+            // Пример добавления путой строки.
             sheet.AddRow();
+            // Пример добавления строки часть которой будет заполнена пустыми строками.
             sheet.AddRow(new List<string>() { "123", "asd" });
+            // Пример добавления строки где часть данных не попадёт в строку.
             sheet.AddRow(
                 new List<string>()
                 {
@@ -79,6 +98,10 @@ namespace TestConsoleApp
             );
         }
 
+        /// <summary>
+        /// Просто метод для отображения данных таблицы в консоли
+        /// </summary>
+        /// <param name="sheet"></param>
         private static void PrintSheet(Sheet sheet)
         {
             Console.WriteLine($"В листе {sheet.Rows.Count} строк");
@@ -99,6 +122,10 @@ namespace TestConsoleApp
             Console.WriteLine();
         }
 
+        /// <summary>
+        /// Получение полномочий для подключения к приложению на Google Cloud Platform.
+        /// </summary>
+        /// <returns></returns>
         private static Stream CredentialStream()
         {
             return new MemoryStream(Properties.Resources.credentials);
