c2bb16992bad4efca1d012538ff5a2d2b43ada3f
PavelAltynnikov <Robenzon11@gmail.com>
2022-01-20 17:41:44 +0400

    add AuthenticationException class

---
+++ src/SynSys.GSpreadsheetEasyAccess.Authentication/AuthenticationException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SynSys.GSpreadsheetEasyAccess.Authentication
+{
+    public class AuthenticationException : Exception
+    {
+        /// <summary>
+        /// Инициализирует новый инстанс AuthenticationException
+        /// </summary>
+        public AuthenticationException() : base() { }
+
+        /// <summary>
+        /// Инициализирует новый инстанс AuthenticationException с сообщением об ошибке
+        /// </summary>
+        public AuthenticationException(string message) : base(message) { }
+
+        /// <summary>
+        /// Инициализирует новый инстанс AuthenticationException с сообщением об ошибке и ссылкой на причину текущего исключения
+        /// </summary>
+        public AuthenticationException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
