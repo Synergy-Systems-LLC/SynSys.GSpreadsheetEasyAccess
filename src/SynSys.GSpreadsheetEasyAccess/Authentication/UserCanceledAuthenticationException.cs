a0ec05f22e68e6e2a9bd3231e6557e7d0b4172f8
PavelAltynnikov <Robenzon11@gmail.com>
2022-02-05 21:17:28 +0400

    add UserCanceledAuthenticationException class

---
+++ src/SynSys.GSpreadsheetEasyAccess/Authentication/UserCanceledAuthenticationException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SynSys.GSpreadsheetEasyAccess.Authentication
+{
+    [Serializable]
+    internal class UserCanceledAuthenticationException : Exception
+    {
+        public UserCanceledAuthenticationException() { }
+
+        public UserCanceledAuthenticationException(string message) : base(message) { }
+
+
+        public UserCanceledAuthenticationException(string message, Exception innerException) : base(message, innerException) { }
+
+        protected UserCanceledAuthenticationException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
