b077abf2b23be36cdca7270316e3602d1ba7e568
PavelAltynnikov <Robenzon11@gmail.com>
2022-02-05 21:16:49 +0400

    add AuthenticationTimedOutException class

---
+++ src/SynSys.GSpreadsheetEasyAccess/Authentication/AuthenticationTimedOutException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SynSys.GSpreadsheetEasyAccess.Authentication
+{
+    [Serializable]
+    internal class AuthenticationTimedOutException : Exception
+    {
+        public AuthenticationTimedOutException() { }
+
+        public AuthenticationTimedOutException(string message) : base(message) { }
+
+
+        public AuthenticationTimedOutException(string message, Exception innerException) : base(message, innerException) { }
+
+        protected AuthenticationTimedOutException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
