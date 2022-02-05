29d29f1f6206e72557289d1b3ae89dcd1f71b692
PavelAltynnikov <Robenzon11@gmail.com>
2022-02-05 21:17:47 +0400

    add InvalidApiKeyException class

---
+++ src/SynSys.GSpreadsheetEasyAccess/Application/InvalidApiKeyException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SynSys.GSpreadsheetEasyAccess.Application
+{
+    [Serializable]
+    internal class InvalidApiKeyException : Exception
+    {
+        public InvalidApiKeyException() { }
+
+        public InvalidApiKeyException(string message) : base(message) { }
+
+        public InvalidApiKeyException(string message, Exception innerException) : base(message, innerException) { }
+
+        protected InvalidApiKeyException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
