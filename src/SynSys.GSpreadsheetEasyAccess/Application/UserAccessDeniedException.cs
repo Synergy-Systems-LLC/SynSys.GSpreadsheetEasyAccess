d4edb2acc7dc7a528f9eed0dc9cb6034c63adad6
PavelAltynnikov <Robenzon11@gmail.com>
2022-02-08 16:05:56 +0400

    add UserAccessDeniedException class

---
+++ src/SynSys.GSpreadsheetEasyAccess/Application/UserAccessDeniedException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SynSys.GSpreadsheetEasyAccess.Application
+{
+    [Serializable]
+    internal class UserAccessDeniedException : Exception
+    {
+        public UserAccessDeniedException() { }
+
+        public UserAccessDeniedException(string message) : base(message) { }
+
+        public UserAccessDeniedException(string message, Exception innerException) : base(message, innerException) { }
+
+        protected UserAccessDeniedException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
