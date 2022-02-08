7d887f0f964159a7fcd342ac07d03d6877791150
PavelAltynnikov <Robenzon11@gmail.com>
2022-02-08 16:15:51 +0400

    add OAuthSheetsScopeException class

---
+++ src/SynSys.GSpreadsheetEasyAccess/Authentication/OAuthSheetsScopeException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SynSys.GSpreadsheetEasyAccess.Authentication
+{
+    [Serializable]
+    internal class OAuthSheetsScopeException : Exception
+    {
+        public OAuthSheetsScopeException() { }
+
+        public OAuthSheetsScopeException(string message) : base(message) { }
+
+        public OAuthSheetsScopeException(string message, Exception innerException) : base(message, innerException) { }
+
+        protected OAuthSheetsScopeException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
