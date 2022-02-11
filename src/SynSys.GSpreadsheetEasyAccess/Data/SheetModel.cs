8bf003313fae019314e81158e2e441793c755cd4
PavelAltynnikov <Robenzon11@gmail.com>
2022-02-11 19:34:19 +0400

    move SheetMode to cs file

--- src/SynSys.GSpreadsheetEasyAccess/Data/SheetModel.cs
+++ src/SynSys.GSpreadsheetEasyAccess/Data/SheetModel.cs
@@ -1,4 +1,4 @@
-using Google.Apis.Sheets.v4.Data;
+﻿using Google.Apis.Sheets.v4.Data;
 using Newtonsoft.Json;
 using SynSys.GSpreadsheetEasyAccess.Data.Exceptions;
 using System;
@@ -9,26 +9,6 @@ using System.Runtime.CompilerServices;
 [assembly:InternalsVisibleTo("SynSys.GSpreadsheetEasyAccess.Tests")]
 namespace SynSys.GSpreadsheetEasyAccess.Data
 {
-    /// <summary>
-    /// Перечисление для определённого заполнения листа.
-    /// Выбор режима влияет на состояние ячеек и строк листа.
-    /// </summary>
-    public enum SheetMode
-    {
-        /// <summary>
-        /// Таблица без шапки и ключа
-        /// </summary>
-        Simple,
-        /// <summary>
-        /// Таблица с шапкой в одну строку
-        /// </summary>
-        Head,
-        /// <summary>
-        /// Таблица с шапкой в одну строку и столбцом-ключём
-        /// </summary>
-        HeadAndKey
-    }
-
     /// <summary>
     /// Тип представляет один лист гугл таблицы.
     /// </summary>
