2944f8095846afba66bf304cbdaf0e51144cb313
PavelAltynnikov <robenzon11@gmail.com>
2022-06-02 18:56:29 +0400

    update SerializationSheetModelConsoleApp example project

--- examples/SerializationSheetModelConsoleApp/Program.cs
+++ examples/SerializationSheetModelConsoleApp/Program.cs
@@ -1,8 +1,10 @@
 ﻿using Newtonsoft.Json;
-using SynSys.GSpreadsheetEasyAccess;
+using SynSys.GSpreadsheetEasyAccess.Application;
+using SynSys.GSpreadsheetEasyAccess.Authentication;
+using SynSys.GSpreadsheetEasyAccess.Data;
 using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Linq;
 
 namespace SerializationSheetModelConsoleApp
 {
@@ -10,58 +12,40 @@ namespace SerializationSheetModelConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"Запуск {nameof(SerializationSheetModelConsoleApp)}\n");
+            Console.WriteLine($"Start {nameof(SerializationSheetModelConsoleApp)}\n");
 
-            var connector = new Connector()
-            {
-                ApplicationName = "Get google sheet data",
-            };
- 
-            bool isConnectionSuccessful = connector.TryToCreateConnect(GetCredentialStream());
+            // Main class for interacting with Google Sheets API
+            var app = new GCPApplication();
 
-            if (!isConnectionSuccessful)
-            {
-                if (connector.Status == ConnectStatus.NotConnected)
-                {
-                    Console.WriteLine($"Подключение отсутствует.\nПричина: {connector.Exception}");
-                }
-                else if (connector.Status == ConnectStatus.AuthorizationTimedOut)
-                {
-                    Console.WriteLine("Время на подключение закончилось");
-                }
+            // To use the application you need to authorize the user.
+            app.AuthenticateAs(new UserAccount(Properties.Resources.credentials, OAuthSheetsScope.FullAccess));
 
-                Console.ReadLine();
- 
-                return;
-            }
+            Console.WriteLine("Authorize completed");
 
-            Console.WriteLine("Подключились к Cloud App\n");
-
+            // For tests, you need to change the given uri to your own.
+            // Don't forget change keyName on GetSheetWithHeadAndKey and ChangeSheet method
             const string uri = "https://docs.google.com/spreadsheets/d/12nBUl0szLwBJKfWbe6aA1bNGxNwUJzNwvXyRSPkS8io/edit#gid=0&fvid=545275384";
 
-            if (connector.TryToGetSheetWithHeadAndKey(uri, "Head 1", out SheetModel sheet))
-            {
-                PrintSheet(sheet, "Первое получение данных");
+            SheetModel sheet = app.GetSheetWithHeadAndKey(uri, "Head 1");
+            PrintSheet(sheet, "Original sheet");
 
-                string jsonSheet = JsonSerialization.SerializeSheet(sheet, Formatting.Indented);
-                Console.WriteLine($"\nЛист после сериализации\n{jsonSheet}");
+            // Serialize SheetModel to string json structure
+            string jsonSheet = JsonSerialization.SerializeSheet(sheet, Formatting.Indented);
+            Console.WriteLine($"\nSheet after serialization\n{jsonSheet}");
 
-                SheetModel deserializedSheet = JsonSerialization.DeserializeSheet(jsonSheet);
-                PrintSheet(deserializedSheet, "Лист после десериализации");
+            // Deserialize string json structure to SheetModel instance
+            SheetModel deserializedSheet = JsonSerialization.DeserializeSheet(jsonSheet);
+            PrintSheet(deserializedSheet, "Sheet after deserialization");
 
-                ChangeSheet(sheet);
-                PrintSheet(sheet, "Изменённые данные до обновления в google");
-
-                connector.UpdateSheet(sheet);
-                PrintSheet(sheet, "Данные после обновления в google");
-            }
-            else
-            {
-                Console.WriteLine(sheet.Status);
-            }
+            // Sheet instance can be used after deserialization
+            ChangeSheet(sheet);
+            PrintSheet(sheet, "Changed sheet before updating to google");
+            app.UpdateSheet(sheet);
+            PrintSheet(sheet, "Sheet after update in google");
 
             Console.ReadLine();
         }
+
 
         private static void ChangeSheet(SheetModel sheet)
         {
@@ -72,53 +56,115 @@ namespace SerializationSheetModelConsoleApp
 
         private static void AddRows(SheetModel sheet)
         {
+            // Add empty row
             sheet.AddRow();
-            sheet.AddRow(new List<string> { "123", "456", "789" });
+
+            // Add a row part of which will be filled with empty cells
+            sheet.AddRow(new List<string>() { "123", "asd" });
+
+            // Add a row where part of the data will not fall into the line, namely "k" and "l",
+            // because test sheet has 10 columns.
+            sheet.AddRow(
+                new List<string>()
+                {
+                    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"
+                }
+            );
         }
 
         private static void ChangeRows(SheetModel sheet)
         {
-            sheet.Rows.Find(row => row.Key.Value == "41")
+            // This example doesn't take into account the absence of a key with the desired value
+            // and a cell with the selected Title
+            sheet.Rows.Find(row => row.Key.Value == "31")
                  .Cells.Find(cell => cell.Title == "Head 6")
-                 .Value = "460";
-            sheet.Rows.Find(row => row.Key.Value == "71")
+                 .Value = "360";
+            sheet.Rows.Find(row => row.Key.Value == "61")
+                 .Cells.Find(cell => cell.Title == "Head 3")
+                 .Value = "630";
+            sheet.Rows.Find(row => row.Key.Value == "51")
                  .Cells.Find(cell => cell.Title == "Head 2")
-                 .Value = "720";
+                 .Value = "520";
+
+            // Change added rows with status RowStatus.ToAppend
+            sheet.Rows.Last().Cells[0].Value = "change";
+            sheet.Rows.Last().Cells[1].Value = "added";
+            sheet.Rows.Last().Cells[2].Value = "line";
         }
 
         private static void DeleteRows(SheetModel sheet)
         {
-            sheet.DeleteRow(sheet.Rows[2]);
-            sheet.DeleteRow(sheet.Rows[8]);
+            // This example doesn't take into account the lack of necessary indexes
+            sheet.DeleteRow(sheet.Rows[3]);
+
+            // Delete the added line with the status RowStatus.ToAppend.
+            // In this case, the line is immediately deleted without waiting for the sheet to be updated.
+            sheet.DeleteRow(sheet.Rows[10]);
         }
 
         private static void PrintSheet(SheetModel sheet, string status)
         {
+            PrintDesctiption(sheet, status);
+            PrintHead(sheet.Head);
+            PrintBody(sheet.Rows);
+        }
+
+        private static void PrintDesctiption(SheetModel sheet, string status)
+        {
             Console.WriteLine(
                 "\n" +
-               $"Статус листа:     {status}\n" +
-               $"Имя таблицы:      {sheet.SpreadsheetTitle}\n" +
-               $"Имя листа:        {sheet.Title}\n" +
-               $"Количество строк: {sheet.Rows.Count}"
+               $"Status:           {status}\n" +
+               $"Spreadsheet Name: {sheet.SpreadsheetTitle}\n" +
+               $"Sheet Name:       {sheet.Title}\n" +
+               $"Number of lines:  {sheet.Rows.Count}\n"
             );
+        }
 
-            foreach (Row row in sheet.Rows)
+        private static void PrintHead(List<string> head)
+        {
+            Console.Write($"{"", 3}");
+
+            foreach (string title in head)
             {
-                Console.Write($"|{row.Number, 3}|");
+                string value = title;
 
-                foreach (Cell cell in row.Cells)
+                if (string.IsNullOrWhiteSpace(title))
                 {
-                    Console.Write($"|{cell.Value, 7}");
+                    value = "-";
+                }
+
+                Console.Write($"|{value,7}");
+            }
+
+            Console.Write($"| ");
+            Console.WriteLine();
+
+            Console.Write($"{"", 3}");
+            var delimiter = new String('-', 7);
+
+            foreach (string title in head)
+            {
+                Console.Write($"|{delimiter}");
+            }
+
+            Console.Write($"| ");
+            Console.WriteLine();
+        }
+
+        private static void PrintBody(List<Row> rows)
+        {
+            foreach (var row in rows)
+            {
+                Console.Write($"{row.Number,3}");
+
+                foreach (var cell in row.Cells)
+                {
+                    Console.Write($"|{cell.Value,7}");
                 }
 
                 Console.Write($"| {row.Status}");
                 Console.WriteLine();
             }
-        }
-
-        private static Stream GetCredentialStream()
-        {
-            return new MemoryStream(Properties.Resources.credentials);
         }
     }
 }
