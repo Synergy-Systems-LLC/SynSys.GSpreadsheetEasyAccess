e1aef22b4b11c992d6a05c86b5672f2b69d4b0a8
PavelAltynnikov <robenzon11@gmail.com>
2022-06-07 17:16:05 +0400

    move RowStatus enum to own module

---
+++ src/SynSys.GSpreadsheetEasyAccess/Data/RowStatus.cs
@@ -0,0 +1,28 @@
+﻿namespace SynSys.GSpreadsheetEasyAccess.Data
+{
+    /// <summary>
+    /// Determines the state of the row.<br/>
+    /// The GCPApplication understands how to work with a specific row (based on this status)
+    /// when sending data to Google spreadsheet.
+    /// </summary>
+    public enum RowStatus
+    {
+        /// <summary>
+        /// Original row from Google spreadsheet.<br/>
+        /// Assigned when loading from Google spreadsheet.
+        /// </summary>
+        Original,
+        /// <summary>
+        /// The row will be changed.
+        /// </summary>
+        ToChange,
+        /// <summary>
+        /// The row will be added.
+        /// </summary>
+        ToAppend,
+        /// <summary>
+        /// The row will be deleted.
+        /// </summary>
+        ToDelete,
+    }
+}
