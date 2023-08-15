5657d194c9083ad5d237a3fabdef3d2117cb1978
PavelAltynnikov <robenzon11@gmail.com>
2023-08-15 18:47:11 +0400

    rename RowStatus enum to ChangeStatus
    Because this status now applies to columns too

---
+++ src/SynSys.GSpreadsheetEasyAccess/Data/ChangeStatus.cs
@@ -0,0 +1,33 @@
+﻿namespace SynSys.GSpreadsheetEasyAccess.Data
+{
+    /// <summary>
+    /// Determines the state of google sheet members.<br/>
+    /// The GCPApplication understands how to work with a specific member (based on this status)
+    /// when sending data to Google spreadsheet or working with model of sheet.
+    /// </summary>
+    // TODO переименовать в ChangingStatus
+    public enum ChangeStatus
+    {
+        /// <summary>
+        /// Original row from Google spreadsheet.<br/>
+        /// Assigned when loading from Google spreadsheet.
+        /// </summary>
+        Original,
+        /// <summary>
+        /// Will be changed.
+        /// </summary>
+        ToChange,
+        /// <summary>
+        /// Will be added.
+        /// </summary>
+        ToAppend,
+        /// <summary>
+        /// Will be inserted.
+        /// </summary>
+        ToInsert,
+        /// <summary>
+        /// Will be deleted.
+        /// </summary>
+        ToDelete,
+    }
+}
