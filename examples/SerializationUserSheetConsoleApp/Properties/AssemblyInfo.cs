1b49bc659efb542490269e4c5b8cbf26eb85806d
PavelAltynnikov <robenzon11@gmail.com>
2023-09-14 16:35:54 +0400

    rename serialization console app example

--- examples/SerializationUserSheetConsoleApp/Properties/AssemblyInfo.cs
+++ examples/SerializationUserSheetConsoleApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SerializationSheetModelConsoleApp")]
+[assembly: AssemblyTitle("SerializationUserSheetConsoleApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SerializationSheetModelConsoleApp")]
+[assembly: AssemblyProduct("SerializationUserSheetConsoleApp")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
