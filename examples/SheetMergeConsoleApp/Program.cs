d1540eedc578b298843ac9badf106ecf3842e37d
PavelAltynnikov <robenzon11@gmail.com>
2023-09-14 19:21:05 +0400

    update sheet merge example

--- examples/SheetMergeConsoleApp/Program.cs
+++ examples/SheetMergeConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Printing;
 using SynSys.GSpreadsheetEasyAccess.Application;
 using SynSys.GSpreadsheetEasyAccess.Application.Exceptions;
 using SynSys.GSpreadsheetEasyAccess.Authentication;
@@ -10,9 +11,9 @@ using System.Collections.Generic;
 
 namespace SheetMergeConsoleApp
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine($"Start {nameof(SheetMergeConsoleApp)}\n");
 
@@ -30,23 +31,24 @@ namespace SheetMergeConsoleApp
                 // Don't forget change keyName on GetSheetWithHeadAndKey and ChangeSheet method
                 const string uri = "https://docs.google.com/spreadsheets/d/12nBUl0szLwBJKfWbe6aA1bNGxNwUJzNwvXyRSPkS8io/edit#gid=0";
 
-                SheetModel sheetForChange = app.GetSheetWithHeadAndKey(uri, "Head 1");
+                UserSheet originalSheet = app.GetUserSheet(new GoogleSheetUri(uri), "Title 1");
+                Printer.PrintSheet(originalSheet, "Original sheet");
 
                 // Create a duplicate of the original sheet
-                SheetModel originalSheet = JsonSerialization.DeserializeSheet(
-                    JsonSerialization.SerializeSheet(sheetForChange, Formatting.None)
+                UserSheet sheetForChange = JsonSerialization.DeserializeSheet<UserSheet>(
+                    JsonSerialization.SerializeSheet(originalSheet, Formatting.None)
                 );
-                PrintSheet(originalSheet, "Original sheet");
 
                 // Add some changes
                 ChangeSheet(sheetForChange);
+                Printer.PrintSheet(sheetForChange, "Changed sheet");
 
                 // Making changes to the original sheet by merging with its modified version
                 originalSheet.Merge(sheetForChange);
-                PrintSheet(originalSheet, "Merged sheet before updating to google");
+                Printer.PrintSheet(originalSheet, "Merged sheet before updating on google");
 
                 app.UpdateSheet(originalSheet);
-                PrintSheet(originalSheet, "Sheet after update in google");
+                Printer.PrintSheet(originalSheet, "Sheet after update in google");
             }
             #region User Exceptions
             catch (AuthenticationTimedOutException)
@@ -132,76 +134,11 @@ namespace SheetMergeConsoleApp
         }
 
 
-        private static void ChangeSheet(SheetModel sheet)
+        private static void ChangeSheet(AbstractSheet sheet)
         {
             sheet.AddRow(new List<string>() { "a", "b", "c", "d", "e" });
             sheet.DeleteRow(sheet.Rows[5]);
             sheet.Rows[1].Cells[1].Value = "22222";
-        }
-
-        private static void PrintSheet(SheetModel sheet, string status)
-        {
-            PrintDescription(sheet, status);
-            PrintHead(sheet.Head);
-            PrintBody(sheet.Rows);
-        }
-
-        private static void PrintDescription(SheetModel sheet, string status)
-        {
-            Console.WriteLine(
-                "\n" +
-               $"Status:            {status}\n" +
-               $"Spreadsheet Title: {sheet.SpreadsheetTitle}\n" +
-               $"Sheet Title:       {sheet.Title}\n" +
-               $"Number of lines:   {sheet.Rows.Count}\n"
-            );
-        }
-
-        private static void PrintHead(List<string> head)
-        {
-            Console.Write($"{"", 3}");
-
-            foreach (string title in head)
-            {
-                string value = title;
-
-                if (string.IsNullOrWhiteSpace(title))
-                {
-                    value = "-";
-                }
-
-                Console.Write($"|{value,7}");
-            }
-
-            Console.Write($"| ");
-            Console.WriteLine();
-
-            Console.Write($"{"", 3}");
-            var delimiter = new String('-', 7);
-
-            foreach (string title in head)
-            {
-                Console.Write($"|{delimiter}");
-            }
-
-            Console.Write($"| ");
-            Console.WriteLine();
-        }
-
-        private static void PrintBody(List<Row> rows)
-        {
-            foreach (var row in rows)
-            {
-                Console.Write($"{row.Number,3}");
-
-                foreach (var cell in row.Cells)
-                {
-                    Console.Write($"|{cell.Value,7}");
-                }
-
-                Console.Write($"| {row.Status}");
-                Console.WriteLine();
-            }
         }
     }
 }
